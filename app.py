2f17ce065695b165a49e137002df44c9a6da5b22
tommytse722 <tommy.tse@live.hk>
2020-09-12 01:21:26 +0800

    Demo

--- app.py
+++ app.py
@@ -7,6 +7,27 @@ from server import app, server
 from flask_login import logout_user, current_user
 from views import success, login, login_fd, logout
 
+import pandas as pd
+
+def get_stock_df():
+  hkex_excel = pd.read_excel('https://www.hkex.com.hk/eng/services/trading/securities/securitieslists/ListOfSecurities.xlsx', skiprows = 2)
+  stock_df = hkex_excel[['Stock Code', 'Name of Securities', 'Board Lot']].copy()
+  stock_df = stock_df.rename(columns={"Stock Code": "stock", "Name of Securities": "name", "Board Lot": "board_lot"})
+  stock_df['stock'] = stock_df['stock'].apply(format_stock)
+  stock_df['board_lot'] = stock_df['board_lot'].apply(format_board_lot)
+  return stock_df
+
+def format_stock(stock):
+  return str(stock).zfill(4) + ".HK"
+
+def format_board_lot(board_lot):
+  return int(board_lot.replace(",", ""))
+
+def download_data_from_yf(period, interval, stock_list):
+  import yfinance as yf
+  data = yf.download(tickers = stock_list, period = period, interval = interval, group_by = 'ticker', auto_adjust = False, prepost = True, threads = True, proxy = None)
+  return data
+
 header = html.Div(
     className='header',
     children=html.Div(
@@ -50,33 +71,26 @@ def display_page(pathname):
         if current_user.is_authenticated:
             layout = html.Div(children=[
                 dcc.Location(id='url_login_success', refresh=True),
-                html.Div(
-                    className="container",
-                    children=[
-                        html.Div(
-                            html.Div(
-                                className="row",
-                                children=[
-                                    html.Div(
-                                        className="ten columns",
-                                        children=[
-                                            html.Br(),
-                                            html.Div(current_user.username),
-                                        ]
-                                    ),
-                                    html.Div(
-                                        className="two columns",
-                                        # children=html.A(html.Button('LogOut'), href='/')
-                                        children=[
-                                            html.Br(),
-                                            html.Button(id='back-button', children='Go back', n_clicks=0)
-                                        ]
-                                    )
-                                ]
-                            )
-                        )
-                    ]
-                )
+                dcc.Dropdown(
+                    options=[
+                        {'label': 'SMA', 'value': 'SMA'},
+                        {'label': 'RSI', 'value': 'RSI'},
+                        {'label': 'MACD', 'value': 'MACD'}
+                    ],
+                    value='SMA'
+                ),
+                dcc.Dropdown(
+                    id='my-dropdown',
+                    options=[{'label': row['stock'] + ' ' + row['name'], 'value': row['stock']} for index, row in get_stock_df().iterrows()],
+                    value='',
+                    multi=True
+                    ),
+            
+                dcc.Input(type='number', value=100000, min=10000, step=10000, style={'width': '110px', 'textAlign': 'right'}),
+            
+                 html.P(children=[
+                    html.Button('Create', id='submit-val', n_clicks=0),
+                    ]),
             ])
             return layout
         else:
