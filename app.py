3ce861f35a62cf6af237be9bd21d7b6e753552c0
tommytse722 <tommy.tse@live.hk>
2020-09-15 22:38:38 +0800

    Demo

--- app.py
+++ app.py
@@ -30,6 +30,11 @@ from plotly.subplots import make_subplots
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 
+#Email
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 def download_data_from_yf(period, interval, stock_list):
     data = yf.download(tickers = stock_list, period = period, interval = interval, group_by = 'ticker', prepost = True)
     return data
@@ -427,13 +432,85 @@ def plot_performance(performance):
 
     title = 'Performance of ' + strategy + ' ( ROI: ' + '{:.2%}'.format(ROI) + ' )' + ' on ' + stock
     fig.update_layout(
-    width=960,
     title_text=title,
     annotations=[dict(text='No. of Trade: '+str(no_of_trade), x=0.13, y=0.5, font_size=18, showarrow=False),
                  dict(text='Net Profit: '+str(total_profit), x=0.9, y=0.5, font_size=18, showarrow=False)])
 
     return fig
 
+def send_html_email(receiver, subject, html_body):
+    sender = "mdascalgotrading@gmail.com"
+    password = "trend2020"
+    message = MIMEMultipart("alternative")
+    message["From"] = sender
+    message["To"] = receiver
+    message["Subject"] = subject
+    message.attach(MIMEText(html_body, "html"))
+    # Create secure connection with server and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender, password)
+        server.sendmail(
+            sender, receiver, message.as_string()
+        )
+    return True
+
+def send_order_signal(receiver, tx_df, performance, execution_date):
+    
+    delta = 0
+    last_tx_date = datetime.strftime(tx_df.index[-1] - timedelta(delta), '%Y-%m-%d')
+    #last_tx_date = '2020-09-02'
+    #execution_date = '2020-09-02'
+
+    if str(last_tx_date)==str(execution_date):
+
+        price = float(tx_df.close[-1])
+        tx_shares = int(tx_df.tx_shares[-1])
+        if tx_shares>0:
+            action = "Buy"
+        else:
+            action = "Sell"
+        tx_shares = abs(tx_shares)
+
+        strategy = str(performance[0])
+        stock = str(performance[1])
+
+        no_of_win = int(performance[2])
+        no_of_loss = int(performance[3])
+        no_of_trade = int(performance[4])
+        win_rate = no_of_win/no_of_trade
+        total_win = float(performance[5])
+        total_loss = float(performance[6])
+        total_profit = float(performance[7])
+        total_cost = float(performance[8])
+        ROI = float(performance[9])
+
+        subject = "{action} {tx_shares} shares of {stock} at ${price}"
+
+        subject = subject.replace('{stock}', stock)
+        subject = subject.replace('{action}', action)
+        subject = subject.replace('{price}', str(price))
+        subject = subject.replace('{tx_shares}', str(tx_shares))
+
+        html_body = """\
+          <html>
+            <body>
+                Win (%): {no_of_win}/{no_of_trade} ({win_rate}) <br>
+                Profit (%):  {total_profit} ({ROI}) <br>
+                by {strategy} on {execution_date}
+            </body>
+          </html>
+          """
+        html_body = html_body.replace('{no_of_win}', str(no_of_win))
+        html_body = html_body.replace('{no_of_trade}', str(no_of_trade))
+        html_body = html_body.replace('{win_rate}', '{:.1%}'.format(win_rate))
+        html_body = html_body.replace('{total_profit}', str(total_profit))
+        html_body = html_body.replace('{ROI}', '{:.1%}'.format(ROI))
+        html_body = html_body.replace('{strategy}', strategy)
+        html_body = html_body.replace('{execution_date}', str(execution_date))
+
+        send_html_email(receiver, subject, html_body)
+        
 app.layout = html.Div(
     [
         header,
@@ -553,6 +630,8 @@ def create_plan(n_clicks, strategy, stocks, capital):
     for stock in stocks:
         plan_mgt.add_plan(current_user.id, strategy, stock, capital)
     tabs = []
+    all_tx_df = pd.DataFrame(columns = ['strategy', 'stock', 'close', 'tx_shares', 'tx_cost', 'shares', 'cash', 'value'])
+    all_trade_df = pd.DataFrame(columns = ['trade_no', 'strategy', 'stock', 'tx_cost', 'net_profit'])
     for stock in stocks:
         graphs = []
         position = eval(strategy)(strategy, stock)
@@ -575,13 +654,6 @@ def create_plan(n_clicks, strategy, stocks, capital):
 
             last_cash = position.loc[item, 'cash']
             last_shares = position.loc[item, 'shares']
-
-        graphs.append(
-            dcc.Graph(
-            id='graph-{}'.format(stock),
-            figure = plot_signals(position)
-            )
-        )
         
         tx_df = get_tx_df(position)
         
@@ -609,11 +681,35 @@ def create_plan(n_clicks, strategy, stocks, capital):
             figure = plot_performance(performance)
             )
         )  
+        
+        graphs.append(
+            dcc.Graph(
+            id='graph-{}'.format(stock),
+            figure = plot_signals(position)
+            )
+        )
+        
         tabs.append(
             dcc.Tab(label=stock, children=graphs)
         )
         
-            
+        send_order_signal(current_user.email, tx_df, performance, date.today())
+        tx_frames = [all_tx_df, tx_df]
+        all_tx_df = pd.concat(tx_frames)
+        trade_frames = [all_trade_df, trade_df]
+        all_trade_df = pd.concat(trade_frames)
+       
+    portfolio_graphs = []
+    portfolio_graphs.append(
+        dcc.Graph(
+        id='performance-{}'.format('Portfolio'),
+        figure = plot_performance(get_performance_df(all_tx_df, all_trade_df))
+        )
+    )  
+    tabs.insert(0, 
+                dcc.Tab(label='Portfolio', children=portfolio_graphs)
+               )
+ 
     return html.Div(dcc.Tabs(tabs))
     #get_plan(current_user.id).to_dict('records')
 
