784c59cc1d09c77529cdf5df751ba79d65027a22
tommytse722 <tommy.tse@live.hk>
2020-09-29 23:57:00 +0800

    Demo

--- app.py
+++ app.py
@@ -494,12 +494,12 @@ def create_plan(stocks, strategy, capital):
     [Input('update-button', 'n_clicks')])
 def update_data(update_clicks):
     if update_clicks>0:
-        sm.drop_stock_table()
-        sm.create_stock_table()
-        sm.download_stock()
-        tm.drop_ticker_table()
-        tm.create_ticker_table()
-        tm.download_ticker()
+#        sm.drop_stock_table()
+#        sm.create_stock_table()
+#        sm.download_stock()
+#        tm.drop_ticker_table()
+#        tm.create_ticker_table()
+#        tm.download_ticker()
     return str(datetime.now())
 
 
