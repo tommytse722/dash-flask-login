00b0041494addb70327f4522480f7b3868e54266
tommytse722 <tommy.tse@live.hk>
2020-10-05 22:44:08 +0800

    Demo

---
+++ LSTM_mgt.py
@@ -0,0 +1,159 @@
+#Import the libraries
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+
+import math
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout
+from datetime import date, datetime, timedelta
+
+import sqlite3
+
+def prediction():
+    ticker_df = get_stock_ticker()
+    for stock in list(ticker_df.code.unique()):
+        get_future_trend(stock, ticker_df[ticker_df.code==stock], 60, 5)
+    
+def get_stock_ticker():
+    conn = sqlite3.connect('database.db')
+    df = pd.read_sql_query("SELECT * FROM ticker", conn)
+    conn.close()
+    df['date'] = pd.to_datetime(df['date'])
+    df = df.set_index('date')
+    return df
+
+def get_next_trading_day(next_trading_day):
+    while next_trading_day.weekday() >= 5:
+        next_trading_day = next_trading_day + timedelta(days=1)     
+    return next_trading_day
+
+def split_data(scaler, signals, days_before):
+    #Create a new dataframe with only the 'Close' column
+    data = signals.filter(['close'])
+    #Converting the dataframe to a numpy array
+    dataset = data.values
+    #Scale the all of the data to be values between 0 and 1 
+    scaled_data = scaler.fit_transform(dataset)
+    #Get /Compute the number of rows to train the model on
+    training_ratio = 0.8
+    training_data_len = math.ceil( len(scaled_data) * training_ratio) 
+    #Create the scaled training data set 
+    train_data = scaled_data[0:training_data_len,:]
+    #Test data set
+    test_data = scaled_data[training_data_len - days_before: , : ]
+    return train_data, test_data
+
+def get_model_data(days_before, data):
+    #Create the x and y data sets
+    x = []
+    y = []
+    for i in range(days_before, len(data)):
+        x.append(data[i-days_before:i,0])
+        y.append(data[i,0])
+    #Convert x and y to a numpy array 
+    x, y = np.array(x), np.array(y)
+    #Reshape the data into the shape accepted by the LSTM
+    x = np.reshape(x, (x.shape[0],x.shape[1],1)) 
+    return x,y
+
+def train_model(data, days_before):
+    X_train, y_train = get_model_data(days_before, data)
+    #Build the LSTM network model
+    model = Sequential()
+    #Adding the first LSTM layer and some Dropout regularisation
+    model.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+    model.add(Dropout(0.2))
+    # Adding a second LSTM layer and some Dropout regularisation
+    model.add(LSTM(units = 50, return_sequences = True))
+    model.add(Dropout(0.2))
+    # Adding a third LSTM layer and some Dropout regularisation
+    model.add(LSTM(units = 50, return_sequences = True))
+    model.add(Dropout(0.2))
+    # Adding a fourth LSTM layer and some Dropout regularisation
+    model.add(LSTM(units = 50))
+    model.add(Dropout(0.2))
+    # Adding the output layer
+    model.add(Dense(units = 1))
+
+    # Compiling the RNN
+    model.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+    # Fitting the RNN to the Training set
+    model.fit(X_train, y_train, epochs = 5, batch_size = 16)
+    return model
+
+def test_model(model, scaler, data, days_before):
+    x_test, y_test = get_model_data(days_before, data)
+    #Getting the models predicted price values
+    prediction = model.predict(x_test) 
+    prediction = scaler.inverse_transform(prediction)#Undo scaling
+    #Calculate/Get the value of RMSE
+    rmse=np.sqrt(np.mean(((prediction - y_test)**2)))
+    print('rmse:'+str(rmse))
+    return prediction
+
+def one_step_ahead(scaler, model, df, days_before):
+    #Get the closing price of past days 
+    past_days = df[-days_before:].values
+    #Scale the data to be values between 0 and 1
+    past_days_scaled = scaler.transform(past_days)
+    #Create an empty list
+    x_test = []
+    #Append the past days
+    x_test.append(past_days_scaled)
+    #Convert the x_test data set to a numpy array
+    x_test = np.array(x_test)
+    #Reshape the data
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+    #Get the predicted scaled price
+    predicted_price = model.predict(x_test)
+    #undo the scaling 
+    predicted_price = scaler.inverse_transform(predicted_price)
+    new_row = pd.DataFrame([float(predicted_price[0][0])], columns=['close'], index=[get_next_trading_day(df.index[-1] + timedelta(days=1))])
+    df = pd.concat([df, pd.DataFrame(new_row)], ignore_index=False)
+    return df
+
+def forecast(scaler, data, model, days_before, days_after):
+    df = data.filter(['close'])
+    for _ in range(days_after):
+        df = one_step_ahead(scaler, model, df, days_before)
+    return df
+
+def plot_data(stock, data, test, future, days_after):
+    #Plot/Create the data for the graph
+    #Visualize the data
+    plt.figure(figsize=(16,8))
+    plt.title('LSTM on ' + stock)
+    plt.xlabel('date', fontsize=18)
+    plt.ylabel('close', fontsize=18)
+    plt.plot(data['close'])
+    plt.plot(test[['close', 'Prediction']])
+    plt.plot(future.tail(days_after+1)['close'])
+    plt.legend(['Train', 'Test', 'Predict', 'Forecast'], loc='lower left')
+    plt.show()
+    
+def get_trend(data, order=1):
+    trend = 0
+    coeffs = np.polyfit(np.arange(len(data)), list(data), order)
+    slope = coeffs[-2]
+    if slope>0:
+        trend=1
+    if slope<0:
+        trend=-1  
+    return int(trend)
+
+def get_future_trend(stock, data, days_before, days_after):
+    scaler = MinMaxScaler(feature_range=(0, 1)) 
+    train_data, test_data = split_data(scaler, data, days_before)
+    model = train_model(train_data, days_before)
+    prediction = test_model(model, scaler, test_data, days_before)
+    test = data[-len(prediction):]
+    test.insert(1, "Prediction", prediction) 
+    future = forecast(scaler, data, model, days_before, days_after)
+    plot_data(stock, data, test, future, days_after)
+    trend = get_trend(future.tail(days_after+1)['close'])
+    return trend
--- optimal_mgt.py
+++ optimal_mgt.py
@@ -5,6 +5,18 @@ from config import engine
 import pandas as pd
 import datetime
 
+import numpy as np
+import sqlite3
+import TA
+import numpy as np
+
+#Data Visualization
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+
 db = SQLAlchemy()
 
 
@@ -33,3 +45,137 @@ def add_optimal(strategy_name, stock_code, capital, x, y):
     conn = engine.connect()
     conn.execute(insert_cmd)
     conn.close()
+    
+def select_plan():
+    conn = sqlite3.connect('database.db')
+    df = pd.read_sql_query("SELECT user_id, strategy_name, stock_code, capital, strategy.* FROM plan, strategy where strategy_name = strategy.name", conn)
+    conn.close()
+    return df
+
+def get_ticker(stock):
+    conn = sqlite3.connect('database.db')
+    stock_code = "'{0}'".format(stock)
+    df = pd.read_sql_query("SELECT stock.code, date, open, high, low, close, adj_close, volume, board_lot FROM stock, ticker where stock.code = " + stock_code + " and stock.code = ticker.code", conn)
+    conn.close()
+    return df
+
+def get_ROI_distribution(r):
+    rows = []
+    for y in range(r.y_start, r.y_end+1, r.y_step):
+        row = []
+        for x in range(r.x_start, r.x_end+1, r.x_step):
+            position, tx_df, trade_df, performance = TA.backtesting(r.stock_code, r.strategy_name, r.capital, get_ticker(r.stock_code), str(x), str(y))
+            ROI = performance[9]
+            #print('x:{}; y:{}; z:{}'.format(x, y, ROI))
+            row.append(ROI)
+        rows.append(row)
+    return rows
+    
+def convolve2D(image, kernel, padding=0, strides=1):
+    # Cross Correlation
+    kernel = np.flipud(np.fliplr(kernel))
+
+    # Gather Shapes of Kernel + Image + Padding
+    xKernShape = kernel.shape[0]
+    yKernShape = kernel.shape[1]
+    xImgShape = image.shape[0]
+    yImgShape = image.shape[1]
+
+    # Shape of Output Convolution
+    xOutput = int(((xImgShape - xKernShape + 2 * padding) / strides) + 1)
+    yOutput = int(((yImgShape - yKernShape + 2 * padding) / strides) + 1)
+    output = np.zeros((xOutput, yOutput))
+
+    # Apply Equal Padding to All Sides
+    if padding != 0:
+        imagePadded = np.zeros((image.shape[0] + padding*2, image.shape[1] + padding*2))
+        imagePadded[int(padding):int(-1 * padding), int(padding):int(-1 * padding)] = image
+    else:
+        imagePadded = image
+
+    # Iterate through image
+    rows = []
+    for x in range(image.shape[0]):
+        # Exit Convolution
+        if x > image.shape[0] - xKernShape:
+            break
+        # Only Convolve if y has gone down by the specified Strides
+        if x % strides == 0:
+            row = []
+            for y in range(image.shape[1]):
+            # Go to next row once kernel is out of bounds
+                if y > image.shape[1] - yKernShape:
+                    break
+                try:
+                    # Only Convolve if x has moved by the specified Strides
+                    if y % strides == 0:
+                        output[x, y] = (kernel * imagePadded[x: x + xKernShape, y: y + yKernShape]).mean()
+                        row.append(output[x, y])
+                except:
+                    break
+        rows.append(row)
+    return rows
+
+def forward_convolution(ROI_distribution):
+    kernel = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+    layers = []
+    length = len(ROI_distribution)
+    output = ROI_distribution
+    layers.append(output)
+    while length > 3:
+        output = convolve2D(np.array(output), kernel)
+        layers.append(output)
+        length = length - 2
+    return layers
+
+def backward_convolution(r, layers):
+    layers.reverse()
+    # For the 1st layer, choose the best one
+    x_start = 0
+    y_start = 0
+    x_end = len(layers[0])
+    y_end = len(layers[0])
+    n = 0
+
+    for layer in layers:
+        #print(layer)
+        max_x = 0
+        max_y = 0
+        max_z = -10000
+        #print('x_start:{}; x_end:{}; y_start:{}; y_end:{}'.format(x_start, x_end, y_start, y_end))
+        n = n + 1
+        if n == len(layers):
+            # For the nth layer, always choose the middle one
+            max_x = x_start + 1
+            max_y = y_start + 1
+            max_z = layer[max_y][max_x]
+        else:
+            # For the 2nd to (n-1)th layer, always choose the middle one
+            for y in range(y_start, y_end):
+                #print(layer[y]) 
+                for x in range(x_start, x_end): 
+                    #print(layer[y][x]) 
+                    if layer[y][x]>max_z:
+                        max_x = x
+                        max_y = y
+                        max_z = layer[y][x]
+            x_start = max_x - 1 + 1
+            y_start = max_y - 1 + 1
+            x_end =   max_x + 1 + 1 + 1
+            y_end =   max_y + 1 + 1 + 1
+    return int((max_x+r.x_start/r.x_step)*r.x_step), int((max_y+r.y_start/r.y_step)*r.y_step), max_z
+
+def plot_ROI_distribution(r, z):
+    x, y = np.linspace(r.x_start, r.x_end, num=int((r.x_end-r.x_start)/r.x_step)+1), np.linspace(r.y_start, r.y_end, num=int((r.y_end-r.y_start)/r.y_step)+1)
+    fig = go.Figure(data=[go.Surface(x=x, y=y, z=z)])
+    fig.update_traces(contours_z=dict(show=True, usecolormap=True, highlightcolor="limegreen", project_z=True))
+    fig.update_layout(title='ROI Distribution', autosize=False, width=700, height=700, scene_camera_eye=dict(x=-2, y=-2, z=0.2))  
+    fig.show()
+    
+def plan_optimization():
+    for _, r in select_plan().iterrows():
+        ROI_distribution = get_ROI_distribution(r)
+        x, y, z = backward_convolution(r, forward_convolution(ROI_distribution))
+        print('The optimal value of (x, y, z) is ({}, {}, {})'.format(x, y, z))
+        plot_ROI_distribution(r, np.array(ROI_distribution))
+        add_optimal(r.strategy_name, r.stock_code, r.capital, x, y)
